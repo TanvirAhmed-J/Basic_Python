935d7b94b5002351f09fe1c608606450d4c44112
TanvirAhmed-J <100840239+TanvirAhmed-J@users.noreply.github.com>
2022-03-03 16:05:13 +0530

    Basic Python programs for practice

--- StringReplace.py
+++ StringReplace.py
@@ -1,10 +1,4 @@
-'''
-* @Author: Mohammad Fatha.
-* @Date: 2021-09-17 00:54  
-* @Last Modified by: Mohammad Fatha
-* @Last Modified time: 2021-09-17 00:58
-* @Title: User Input and Replace String Template “Hello <<UserName>>, How are you?
-'''
+
 import re
 
 str = "Hello <<UserName>>, How are you?"
