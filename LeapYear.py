22b697d7bc796e4d839d5ed060a7572974599745
TanvirAhmed-J <100840239+TanvirAhmed-J@users.noreply.github.com>
2022-03-03 16:02:41 +0530

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,11 +1,4 @@
 
-'''
-* @Author: Mohammad Fatha.
-* @Date: 2021-09-17 00:00  
-* @Last Modified by: Mohammad Fatha
-* @Last Modified time: 2021-09-17 00:05
-* @Title: To check the entred year is leap year or not
-'''
 import re
 
 Year = int(input("Enter the year to check it is leap year or not : "))
